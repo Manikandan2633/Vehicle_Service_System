d3ce494a4d463ba9bb6b1f662421ad79cca4c874
Manikandan <117503976+Manikandan2633@users.noreply.github.com>
2023-09-18 11:01:06 +0530

    Add files via upload

---
+++ login_form.php
@@ -0,0 +1,88 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+  
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
+
+      if($row['user_type'] == 'admin'){
+
+         $_SESSION['admin_name'] = $row['name'];
+         header('location:admin/index.php');
+
+      }
+      elseif($row['user_type'] == 'user'){
+
+        $_SESSION['user_name'] = $row['name'];
+         header('location:user/index.php');
+      }
+     
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login form</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="login.css">
+
+</head>
+<body>
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+      <p>Don't have an account? <a href="register_form.php">Create one</a></p>
+   </form>
+
+</div>
+
+
+
+
+            <footer class="sticky-footer bg-white">
+                <div class="container-fluid mt-3  ">
+                    <div class="copyright text-center mb-3 text-black">
+                        <h4>Copyright &copy; AUTOCARE</h4>
+                    </div>
+                </div>
+            </footer> 
+
+
+
+
+</body>
+</html>
